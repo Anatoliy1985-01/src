8c66260ceab961415950cf1e207b5fe1e74127b3
anatoliy <lisovoya85@gmail.com>
2024-04-27 20:48:26 +0300

    nodejs22

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-const gulp        = require('gulp');
+const gulp        = recuire('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = import('gulp-autoprefixer');
 const rename = require("gulp-rename");
 
 gulp.task('server', function() {
